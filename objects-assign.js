a56fc9eee5ce239643c7de4278bda65312361810
Bonifase <orwabonifase@gmail.com>
2019-04-01 21:45:59 +0300

    working with sets

--- objects-assign.js
+++ objects-assign.js
@@ -1,6 +1,14 @@
+// Object.assign
 let options = {name: "bonny", gender: "male"};
 
 let defaults = {role: "Developer"};
 
 let settings = Object.assign({}, defaults, options);
-console.log(settings)
+console.log(settings);
+
+// Arrays
+let users = ['sam', 'tyler', 'brooke'];
+
+for(let name of users){
+    console.log(name);
+}
